3b55e048bc63ef8f924d9a6b7325c41626ad3fcc
沈涛 <378606672@qq.com>
2022-09-27 21:04:05 +0800

    模型训练

--- train_gpu_1.py
+++ train_gpu_1.py
@@ -1,16 +1,15 @@
 import torch.optim
 import torchvision
+import time
 from torch import nn
 
 from torch.utils.tensorboard import SummaryWriter
 from torch.nn import ReLU
-
-#准备数据集
-#训练数据集
 from torch.utils.data import DataLoader
 
 from model import Shentao
-
+#准备数据集
+#训练数据集
 train_data = torchvision.datasets.CIFAR10(root = "../data_gpu", train=True, transform=torchvision.transforms.ToTensor(), download = True)
 #测试数据集
 test_data = torchvision.datasets.CIFAR10(root= "../data_gpu", train=False, transform=torchvision.transforms.ToTensor(), download = True)
@@ -26,7 +25,7 @@ print("测试数据集长度为：{}".format(test_data_size))
 train_dataloader = DataLoader(train_data, batch_size=64)
 test_dataloader = DataLoader(test_data, batch_size=64)
 
-#创建网络模型
+#搭网络
 class Shentao(nn.Module):
     #初始化
     def __init__(self):
@@ -56,6 +55,8 @@ class Shentao(nn.Module):
     def forward(self, x):
         x = self.model(x)
         return x
+
+# 创建网络模型
 shentao = Shentao()
 shentao = shentao.cuda()
 #损失函数
@@ -69,12 +70,13 @@ optimizer = torch.optim.SGD(shentao.parameters(), lr=learning_rate)
 #记录训练次数
 total_train_step = 0
 #记录测试次数
-total_test_step =0
+total_test_step = 0
 #训练的轮数
-epoch = 10
+epoch = 5
 
 writer = SummaryWriter("train_gpu_1")
 
+start_time = time.time()
 for i in range(epoch):
     print("-----------第{}轮训练开始------------".format(i+1))
     #训练开始
@@ -83,32 +85,38 @@ for i in range(epoch):
         imgs, tragets = data
 
         device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
         imgs = imgs.cuda()
         targets = tragets.cuda()
         tragets = tragets.to(device)
+
         outputs = shentao(imgs)
-        print(outputs.is_cuda)
-        print(tragets.is_cuda)
-        loss = loss_fn(outputs, tragets)
+        loss = loss_fn(outputs, tragets)#(预测的输出，真实)
 
         #优化器优化模型
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
+        optimizer.zero_grad()#梯度清零
+        loss.backward()#损失进行反向传播
+        optimizer.step()#参数进行优化
 
         total_train_step = total_train_step + 1
-        if total_test_step % 100 == 0:
+        if total_train_step % 100 == 0:
+            end_time = time.time()
+            print(end_time - start_time)
             print("训练次数：{},loss:{}".format(total_train_step, loss))
-            writer.add_scalar("train_loss", loss.item(), total_test_step)
+            writer.add_scalar("train_loss", loss.item(), total_train_step)
+
+
     #测试开始
     shentao.eval()## 将模型设置成验证状态
     total_test_loss = 0
     total_accurary = 0
+
     with torch.no_gard():## 对梯度清零，防止上一轮的梯度影响下一轮的学习
         for data in test_dataloader:
             imgs, tragets = data
 
             device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
             imgs = imgs.cuda()
             targets = tragets.cuda()
             tragets = tragets.to(device)
@@ -120,13 +128,13 @@ for i in range(epoch):
             accurary = (outputs.argmax(1) == tragets).sum()
             total_accurary = total_accurary + accurary
 
-        print("整体测试集上的Loss:{}".format(total_test_loss))
-        print("整体测试集上的准确率:{}".format(total_accurary/test_data_size))
-        writer.add_scalar("test_loss", total_test_loss, total_test_step)
-        writer.add_scalar("test_accurary", total_accurary/test_data_size, total_test_step)
-        total_test_step  = total_test_step + 1
+    print("整体测试集上的Loss:{}".format(total_test_loss))
+    print("整体测试集上的准确率:{}".format(total_accurary/test_data_size))
+    writer.add_scalar("test_loss", total_test_loss, total_test_step)
+    writer.add_scalar("test_accurary", total_accurary/test_data_size, total_test_step)
+    total_test_step  = total_test_step + 1
 
-        torch.save(shentao,"shentao_{}.pth".format(i))
-        print("模型已保存")
+    torch.save(shentao, "shentao_{}.pth".format(i))
+    print("模型已保存")
 
 writer.close()
